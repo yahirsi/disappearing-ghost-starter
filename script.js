bf8c132174c3f084ddb2af08a8e2788c4bb9c1e2
Glitch (warm-bramble-lamb) <none>
2020-12-01 22:24:50 +0000

    🐛🍯 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+$(".hide-button").click(function() {
+  $(".ghost-image").hide();
+});
+$(".show-button").click(function() {
+  $(".ghost-image").show();
+});
 
+$(".update-img-button").click(function() {
+  $(".ghost-image").css("transform", "rotate(90deg)");
+});
+$(".message-button").click(function() {
+  let Bill = $(".input").val();
+  $(".message").append(Bill+"<br>");
+});
+$(".name-button").click(function() {
+   let Bill = $(".input").val();
+  $(".h1").text("Hi I'm "+Bill);
+});
